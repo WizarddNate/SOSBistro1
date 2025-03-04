675fd733d48e1dbf2aaa297089af0efbd3189f60
CoolACT74 <actobias007@gmail.com>
2025-03-03 18:24:21 -0600

    Added Music LVL 1

---
+++ SOS Bistro/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public AudioSource levelMusic;
+
+    public bool startPlaying;
+
+    //script for moving the notes that i havent made yet
+    //public NoteScroller ns;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
